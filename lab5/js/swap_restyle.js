df50945d6e88e784cf040160f58c46c81007e202
Martin White <m.white@sussex.ac.uk>
2019-03-05 14:38:47 +0000

    Added JavaScript to conver 4 page site to SPA

---
+++ lab5/js/swap_restyle.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+var counter = 0;
+
+function swap(selected) {
+	<!-- First do not display all div id contents -->
+	document.getElementById('home').style.display = 'none';
+	document.getElementById('coke').style.display = 'none';
+	document.getElementById('sprite').style.display = 'none';
+	document.getElementById('pepper').style.display = 'none';
+	
+	<!-- Then display the selected div id contents -->	
+	document.getElementById(selected).style.display = 'block';
+}
+function changeLook() {
+	counter += 1;
+	switch (counter) {
+        case 1:
+            document.getElementById('bodyColor').style.backgroundColor = 'lightblue';
+			document.getElementById('headerColor').style.backgroundColor = '#FF0000';
+			document.getElementById('footerColor').style.backgroundColor = '#FF9900';
+            break;
+        case 2:
+			document.getElementById('bodyColor').style.backgroundColor = '#FF6600';
+			document.getElementById('headerClor').style.backgroundColor = '#FF9999';
+			document.getElementById('footerColor').style.backgroundColor = '#996699';
+            break;
+        case 3:
+			document.getElementById('bodyColor').style.backgroundColor = 'coral';
+			document.getElementById('headerColor').style.backgroundColor = 'darkcyan';
+			document.getElementById('footerColor').style.backgroundColor = 'darksalmom';
+            break;
+        case 4:
+            counter = 0;
+			document.getElementById('bodyColor').style.backgroundColor = 'lightgrey';
+			document.getElementById('headerColor').style.backgroundColor = 'chocolate';
+			document.getElementById('footerColor').style.backgroundColor = 'dimgrey';
+            break;
+    }
+}
+
+function changeBack() {
+	document.getElementById('bodyColor').style.backgroundColor = '#FFFFFF';
+	document.getElementById('headerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+	document.getElementById('footerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+}
